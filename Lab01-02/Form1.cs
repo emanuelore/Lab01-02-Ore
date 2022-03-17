6a6ac93446ec283d560c3d0d8ee35572c0df3cba
emanuelore <emanuel.ore@tecsup.edu.pe>
2022-03-16 23:40:25 -0500

    Cambios agregados

---
+++ Lab01-02/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int n = r.Next(10000,100000);
+            char l = (char)r.Next('A', 'Z');
+            string codigo = Char.ToString(l) + n;
+            string dni = txtDni.Text;
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string direccion = txtDireccion.Text;
+            string telefono = txtTelefono.Text;
+            string email = txtEmail.Text;
+            string fn = fecha.Text;
+            string departamento = txtDepartamento.Text;
+            string cargo = txtCargo.Text;
+            
+            dgvUsuarios.Rows.Add(codigo, dni, nombre, apellido, direccion, telefono, email, fn, departamento, cargo );
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
